d2e1aeafcb520f929608e5555761fe7c563f12ef
olygood <olivier.huttmacher@gmail.com>
2019-06-12 14:59:07 +0200

    move perso en GetAxis et keyCode time.deltatime*speed

--- donjon2019/Assets/Scripts/MovePerso.cs
+++ donjon2019/Assets/Scripts/MovePerso.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class MovePerso : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int speed = 3;
+    public int acceleration = 20;
     void Start()
     {
-        
+
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        /* vitesse directe getKey
+        if (Input.GetKey(KeyCode.UpArrow)) 
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+    */
+
+        // vitesse plus souple getAxis
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * v * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * h * Time.deltaTime * speed);
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * v * Time.deltaTime * acceleration);
+            transform.Translate(Vector3.right * h * Time.deltaTime * acceleration);
+        }
     }
+    
 }
+
+
+// même chose
+// transform.translate(new vector3(0;0,1));
+// transform.translate(Vector3.forward);
