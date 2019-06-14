cf8e61e3a07236c317326065592565e4530ac7a0
olygood <olivier.huttmacher@gmail.com>
2019-06-14 16:19:13 +0200

    script raycast

---
+++ donjon2019/Assets/Scripts/Raycast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, Vector3.forward * 20, Color.cyan, 1);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+        {
+            if (hit.transform.tag == "enemy1")
+            {
+                Destroy(hit.transform.gameObject,3);
+            }
+        }
+    }           
+
+}
