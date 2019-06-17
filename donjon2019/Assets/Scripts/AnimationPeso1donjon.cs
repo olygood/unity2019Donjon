fc5e9b626d6209fda60dbd0c4f0da45636ad1371
olygood <olivier.huttmacher@gmail.com>
2019-06-17 21:31:26 +0200

    animation et animator

---
+++ donjon2019/Assets/Scripts/AnimationPeso1donjon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPeso1donjon : MonoBehaviour
+{
+    Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+            anim.SetBool("laConditionSaut",true);
+            }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetBool("laConditionSaut", false);
+        }
+    }
+}
