b121ab6640dce9b997fab213c205916346b2597f
olygood <olivier.huttmacher@gmail.com>
2019-06-21 16:46:40 +0200

    add pro builder textmesh pro and pro grid

---
+++ donjon2019/Assets/Scripts/UI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FinText());
+    }
+
+    IEnumerator FinText()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
